004cd20182535a1e78eff0f8837c46686df86e56
VARSHA S <ktmvarsh04@gmail.com>
2025-08-05 16:01:40 +0530

    done

--- tree Sums/binary search tree.py
+++ tree Sums/binary search tree.py
@@ -11,19 +11,35 @@ class binarySearchTree:
     def add(self,data):
         if self.root is None:
             self.root = BSTNode(data)
+        else:
+            self.recursive_add(data,self.root)
     
     def recursive_add(self,data,node):
         if data < node.data:
-            if node.left is not None:
+            if node.left is  None:
                 node.left = BSTNode(data)
             else:
                 self.recursive_add(data,node.left)
 
         if data>node.data:
-            if node.right is not None:
+            if node.right is  None:
                 node.right = BSTNode(data)
             else:
-                self.recursive_add(data,self.right)
+                self.recursive_add(data,node.right)
+    
+    def display(self):
+        result =[]
+        self.inorderTraversal(self.root,result)
+        print(result)
+
+    def inorderTraversal(self,node,result):
+        if node is None:
+            return None
+        else:
+            self.inorderTraversal(node.left,result)
+            result.append(node.data)
+            self.inorderTraversal(node.right,result)
+
 
 BST = binarySearchTree()
 BST.add(45)
@@ -34,3 +50,4 @@ BST.add(87)
 BST.add(23)
 BST.add(1)
 BST.add(67)
+BST.display()
